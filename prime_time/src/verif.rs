5ba021b49025855af44161d056a50867fb16f09b
aaronjeline <aaroneline@icloud.com>
2025-07-04 13:48:19 -0400

    forgot to optimize

--- prime_time/src/verif.rs
+++ prime_time/src/verif.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
+use std::time;
 use thiserror::Error;
+use tracing::info;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Request {
@@ -13,17 +15,20 @@ pub enum Error {
     InvalidMethod(String),
 }
 
+#[tracing::instrument(skip(buf))]
 pub fn process_request(buf: &[u8]) -> anyhow::Result<Vec<u8>> {
+    let time = time::Instant::now();
     let parsed: Request = serde_json::from_slice(buf)?;
     let resp = parsed.process()?;
     let buf = serde_json::to_vec(&resp)?;
+    info!("Processed request in {} ms", time.elapsed().as_millis());
     Ok(buf)
 }
 
 impl Request {
     pub fn process(self) -> anyhow::Result<Response> {
         if self.method == "isPrime" {
-            Ok(Response::new(is_prime(self.number)))
+            Ok(Response::new(is_prime_opt(self.number)))
         } else {
             Err(Error::InvalidMethod(self.method))?
         }
@@ -43,6 +48,20 @@ fn is_prime(x: i64) -> bool {
     }
 }
 
+fn is_prime_opt(x: i64) -> bool {
+    if x <= 1 {
+        return false;
+    } else {
+        let bound = x.isqrt() + 1;
+        for i in 2..bound {
+            if x % i == 0 {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Response {
     method: &'static str,
@@ -54,6 +73,17 @@ impl Response {
         Self {
             method: "isPrime",
             prime,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use proptest::prelude::*;
+    proptest! {
+        #[test]
+        fn is_prime_equivalence(x in -10000 as i64..10000 as i64) {
+            prop_assert_eq!(super::is_prime(x), super::is_prime_opt(x));
         }
     }
 }
